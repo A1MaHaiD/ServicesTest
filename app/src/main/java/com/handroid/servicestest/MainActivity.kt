62c63026b086dde5b1d08401e3666713e915eed9
Alexandr <super.lukianchuk2010@gmail.com>
2022-01-19 20:57:22 +0200

    created MyService.kt && add timer inside

--- app/src/main/java/com/handroid/servicestest/MainActivity.kt
+++ app/src/main/java/com/handroid/servicestest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.handroid.servicestest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.handroid.servicestest.databinding.ActivityMainBinding
+import com.handroid.servicestest.services.MyService
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.tvSimpleService.setOnClickListener {
+           startService(MyService.newIntent(this))
+        }
     }
 }
