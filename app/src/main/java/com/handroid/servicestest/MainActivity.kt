223b03dbe5db80b9d475a7b145573e4c728eaa19
Alexandr <super.lukianchuk2010@gmail.com>
2022-01-19 18:10:47 +0200

    added layout && dependency && binding && resource

--- app/src/main/java/com/handroid/servicestest/MainActivity.kt
+++ app/src/main/java/com/handroid/servicestest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.handroid.servicestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.handroid.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
