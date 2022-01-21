acf405e35cb15e4a2e47e20ae96463d8c8a943ff
Alexandr <super.lukianchuk2010@gmail.com>
2022-01-21 02:54:18 +0200

    resolve enqueue inside MyIntentService.kt

--- app/src/main/java/com/handroid/servicestest/services/MyJobService.kt
+++ app/src/main/java/com/handroid/servicestest/services/MyJobService.kt
@@ -1,7 +1,9 @@
 package com.handroid.servicestest.services
 
-import  android.app.job.JobParameters
+import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.Build
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -16,12 +18,20 @@ class MyJobService : JobService() {
 
     override fun onStartJob(parameters: JobParameters?): Boolean {
         log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = parameters?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    parameters?.completeWork(workItem)
+                    workItem = parameters?.dequeueWork()
+                }
+                jobFinished(parameters, false)
             }
-            jobFinished(parameters,true)
         }
         return true
     }
@@ -45,5 +55,12 @@ class MyJobService : JobService() {
         const val SERVICE_TAG = "SERVICE_TAG"
         private const val NAME = "MyJobService"
         const val JOB_ID = 1
+        const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
